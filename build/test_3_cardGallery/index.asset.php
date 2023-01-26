03879a80a1414befa6e46aabe09c52aba9f3075d
Adrien-Ray <a.raymond@codeur.online>
2023-01-26 14:43:55 +0100

    ??

--- build/test_3_cardGallery/index.asset.php
+++ build/test_3_cardGallery/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'd6af33cbe27efc1a3a64');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '98bc8f0586731e20cfb3');
